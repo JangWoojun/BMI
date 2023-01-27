8ff5fe4b14e747bc19b70d80d0b76b1ccf861c60
JangWoojun <woojun0107@naver.com>
2023-01-27 15:35:41 +0900

    feat: TextField 부가 기능 추가
    TextField 클릭 시 배경 변경 및 배경 클릭 시키보드 내리기 추가

--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -1,19 +1,47 @@
-//
-//  ViewController.swift
-//  BMI
-//
-//  Created by Jangwoojun on 2023/01/27.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var mainButton: UIButton!
+    
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        UISetUp()
+    }
+    
+    func UISetUp() {
+        heightTextField.delegate = self
+        weightTextField.delegate = self
+        
+        heightTextField.borderStyle = .roundedRect
+        weightTextField.borderStyle = .roundedRect
     }
 
-
+    @IBAction func mainButtonTapped(_ sender: UIButton) {
+        
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        view.backgroundColor = .systemGray4
+        heightTextField.backgroundColor = .systemGray4
+        weightTextField.backgroundColor = .systemGray4
+        
+        textField.backgroundColor = .white
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        view.backgroundColor = .white
+        heightTextField.backgroundColor = .white
+        weightTextField.backgroundColor = .white
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+         self.view.endEditing(true)
+   }
+    
 }
 
