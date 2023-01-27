92b80859f699f4e7f7ab8c8993fcc751cf82c4fe
JangWoojun <woojun0107@naver.com>
2023-01-27 16:25:34 +0900

    feat: BMI 계산 로직
    bmi 계산 로직 구현

--- BMI/ResultViewController.swift
+++ BMI/ResultViewController.swift
@@ -2,11 +2,25 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var bmiTextLabel: UILabel!
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    var bmi: String?
+    var bmiText: String?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        UISetUp()
     }
     
+    func UISetUp() {
+        bmiLabel.text = bmi
+        bmiTextLabel.text = bmiText
+    }
 
-
+    @IBAction func backButton(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
+    
 }
--- BMI/ViewController.swift
+++ BMI/ViewController.swift
@@ -22,7 +22,30 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
 
     @IBAction func mainButtonTapped(_ sender: UIButton) {
+        guard let resultVC = storyboard?.instantiateViewController(withIdentifier: "ResultVC") as? ResultViewController else {
+            return
+        }
         
+        if let height = Double(heightTextField.text!), let weight = Double(weightTextField.text!) {
+                
+            let bmi = weight / ((height * 0.01) * (height * 0.01))
+            
+            resultVC.bmi = String(format: "%.1f", bmi)
+            
+            if bmi < 18.5 {
+                resultVC.bmiText = "저체중으로 체중 증량이 필요합니다"
+            } else if bmi < 23 {
+                resultVC.bmiText = "정상 체중입니다"
+            } else if bmi < 25 {
+                resultVC.bmiText = "과체중으로 약간에 체중 감량이 필요합니다"
+            } else {
+                resultVC.bmiText = "비만으로 체중 감량이 필요합니다"
+            }
+            
+            present(resultVC, animated: true)
+        
+        }
+                
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
